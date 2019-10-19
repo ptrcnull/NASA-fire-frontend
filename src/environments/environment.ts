e354ed335ac1202b90132b49614593520daadbdf
Bjornskjald <github@bjorn.ml>
2019-10-19 10:50:36 +0200

    Changed code style

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false
-};
-
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
