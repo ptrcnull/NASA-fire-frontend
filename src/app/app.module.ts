1e06ce9c1c44d00ddd1cf1c2af38b9cef90d1793
Michal <m.matera09@wp.pl>
2019-10-19 15:33:19 +0200

    added category icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,25 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
-import { MatIconModule, MatToolbarModule } from '@angular/material'
-import { NavbarComponent } from './components/navbar/navbar.component'
+import { MatButtonModule, MatCardModule, MatIconModule, MatToolbarModule } from '@angular/material'
+import { FireListComponent } from './components/fire-list/fire-list.component'
 import { MainComponent } from './components/main/main.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    NavbarComponent
+    NavbarComponent,
+    FireListComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
---
+++ src/app/components/fire-list/fire-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-fire-list',
+  templateUrl: './fire-list.component.html',
+  styleUrls: ['./fire-list.component.scss']
+})
+export class FireListComponent implements OnInit {
+  fires = [
+    {
+      startDate: '01/02/2019',
+      fireNotificationApproveCounter: '',
+      fireTeam: false,
+      isFire: false,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, eveniet?',
+      fireDepartamentDescription: '',
+      arePeople: false,
+      isWood: false,
+      isBuilding: false,
+      isHazardousMaterial: false,
+      isElectricity: false
+    },
+    {
+      startDate: '01/02/2019',
+      fireNotificationApproveCounter: '',
+      fireTeam: true,
+      isFire: true,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, eveniet?',
+      fireDepartmentDescription: '',
+      arePeople: true,
+      isWood: true,
+      isBuilding: true,
+      isHazardousMaterial: true,
+      isElectricity: true
+    }]
+
+  constructor () {
+  }
+
+  ngOnInit () {
+  }
+
+}
